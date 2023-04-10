e08bc08519582bc05421ef7c430fa3089e604fb8
George Israel <georgeisrael22@gmail.com>
2023-04-10 16:34:26 -0700

    1-create_file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,25 +11,24 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	 int fd;
+	int fd, w, len = 0;
 
-	 if (filename == NULL) {
-	 return (-1);
-    }
+	if (filename == NULL)
+		return (-1);
 
-   	 fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-    	 if (fd == -1) {
-         return (-1);
-    }
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
 
-	    if (text_content != NULL) {
-            ssize_t len = strlen(text_content);
-            ssize_t written = write(fd, text_content, len);
-            if (written != len) {
-            close(fd);
-            return (-1);         }
-    }
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
 
-    close(fd);
-    return (1);
+	if (fd == -1 || w == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
 }
