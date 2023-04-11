89bc32cd8213601ee4a6c3ff24a3ee42a00d2137
George Israel <georgeisrael22@gmail.com>
2023-04-10 17:52:15 -0700

    0-read_textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,26 +6,23 @@
  * @filename: is used to specify the file to read
  * @letters: number of letters to be read
  * Return (w) - The function returns the actual number of bytes read and printed.
- * If the function fails or `filename` is NULL, it returns 0
+ * If the function fails or filename is NULL, it returns 0
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	 char *buf;
-	 ssize_t fd;
-	 ssize_t w;
-   	 ssize_t t;
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
 
-    	fd = open(filename, O_RDONLY);
-    	if (fd == -1)
-       		 return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
 
-   	buf = malloc(sizeof(char) * letters);
-     
-    t = read(fd, buf, letters);
-    w = write(STDOUT_FILENO, buf, t);
-
-    free(buf);
-    close(fd);
-
-    return (w);
+	free(buf);
+	close(fd);
+	return (w);
 }
