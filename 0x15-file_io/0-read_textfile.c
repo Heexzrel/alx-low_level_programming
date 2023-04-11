3ffbd875954d5ecb09e0571cc6c38aacd2275f80
George Israel <georgeisrael22@gmail.com>
2023-04-10 17:32:13 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,7 +17,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
     	fd = open(filename, O_RDONLY);
     	if (fd == -1)
-        return (0);
+       		 return (0);
 
    	buf = malloc(sizeof(char) * letters);
      	if (buf == NULL) {
@@ -30,5 +30,5 @@ ssize_t read_textfile(const char *filename, size_t letters)
     free(buf);
     close(fd);
 
-    return w;
+    return (w);
 }
