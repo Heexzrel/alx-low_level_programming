ecdbe71a9a26a8a87036e33553af931e7a7bf7b8
George Israel <georgeisrael22@gmail.com>
2023-04-10 18:02:29 -0700

    2-append_text_to_file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -6,7 +6,6 @@
  * @text_content: text to append to the file
  * Return: 1 on success, -1 on failure
  */
-
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int o, w, len = 0;
@@ -22,9 +21,9 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	o = open(filename, O_WRONLY | O_APPEND);
 	w = write(o, text_content, len);
-	
+
 	if (o == -1 || w == -1)
-	   return (-1);
+		return (-1);
 
 	close(o);
 
