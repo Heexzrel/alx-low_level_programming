39a372cd2008495c7897105e699dcb2f4f4b75d5
George Israel <georgeisrael18@gmail.com>
2023-05-04 15:02:11 -0700

    1-print_binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -12,18 +12,3 @@ void print_binary(unsigned long int h)
 
 	_putchar((h & 1) + '0');
 }
-
-#include "main.h"
-
-/**
- * print_binary - Prints the binary rep of a number
- * @n: The number to be in binary.
- */
-
-void print_binary(unsigned long int h)
-{
-	if (h > 1)
-		print_binary(h >> 1);
-
-	_putchar((h & 1) + '0');
-}
